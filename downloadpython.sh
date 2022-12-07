41a9c301beb2098df7c5e0d53763770cdba4b706
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2022-12-07 14:50:32 +0200

    Update downloadpython.sh

--- downloadpython.sh
+++ downloadpython.sh
@@ -1,13 +1,12 @@
 url='https://www.python.org/ftp/python/'
 
-
 curl --silent "$url" |
     sed -n 's!.*href="\([0-9]\+\.[0-9]\+\.[0-9]\+\)/".*!\1!p' |
     sort -rV |
 while read -r version; do
     filename="Python-$version.tgz"
     # Versions which only have alpha, beta, or rc releases will fail here.
-    # Stop when we find one with a final release.
+    # Stop when we find one with a final release
     if curl --fail --silent -O "$url/$version/$filename"; then
         echo "$filename"
         break
